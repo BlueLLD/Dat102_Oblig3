8e46bae13d09bd86deec1c715cde54362215308c
DrTellus <102365316+DrTellus@users.noreply.github.com>
2026-03-10 14:28:21 +0100

    Lagt til alt på nytt

--- src/LenketMengde.java
+++ src/LenketMengde.java
@@ -1,5 +1,70 @@
 public class LenketMengde<T> implements MengdeADT<T> {
 
+    @Override
+    public boolean erTom() {
+        return false;
+    }
+
+    @Override
+    public boolean inneholder(T element) {
+        return false;
+    }
+
+    @Override
+    public boolean erDelmengdeAv(MengdeADT<T> annenMengde) {
+        return false;
+    }
+
+    @Override
+    public boolean erLik(MengdeADT<T> annenMengde) {
+        return false;
+    }
+
+    @Override
+    public boolean erDisjunkt(MengdeADT<T> annenMengde) {
+        return false;
+    }
+
+    @Override
+    public MengdeADT<T> snitt(MengdeADT<T> annenMengde) {
+        return null;
+    }
+
+    @Override
+    public MengdeADT<T> union(MengdeADT<T> annenMengde) {
+        return null;
+    }
+
+    @Override
+    public MengdeADT<T> minus(MengdeADT<T> annenMengde) {
+        return null;
+    }
+
+    @Override
+    public void leggTil(T element) {
+
+    }
+
+    @Override
+    public void leggTilAlleFra(MengdeADT<T> annenMengde) {
+
+    }
+
+    @Override
+    public T fjern(T element) {
+        return null;
+    }
+
+    @Override
+    public T[] tilTabell() {
+        return null;
+    }
+
+    @Override
+    public int antallElementer() {
+        return 0;
+    }
+
     private class Node {
         T data;//implementerer variabel data av typen generisk T (Verdien)
         Node neste;//og variabel neste av typen Node(peker til neste node)
