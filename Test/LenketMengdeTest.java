b234de4c45b9db6085cf60352c04f36803cbfdaa
Lars <lduinker04@gmail.com>
2026-03-19 11:39:28 +0100

    Removed old tests

--- Test/LenketMengdeTest.java
+++
@@ -1,7 +0,0 @@
-public class LenketMengdeTest extends MengdeTest{
-
-    @Override
-    protected MengdeADT<String> lagNyMengde(){
-        return new LenketMengde<>();
-    }
-}
--- Test/TabellTest.java
+++
@@ -1,8 +0,0 @@
-public class TabellTest extends MengdeTest {
-    @Override
-    protected MengdeADT<String> lagNyMengde() {
-        return new TabellMengde<>();
-    }
-
-}
-
