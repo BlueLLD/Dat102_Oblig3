b573355d53685608ae35a1852af9cee83a933dd0
Lars <lduinker04@gmail.com>
2026-03-19 12:22:18 +0100

    Oppgave 4.f

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,21 @@
 void main() {
-	
+	Person p1 = new Person("Arne", "sage", "tulle", "smile", "lese");
+	Person p2 = new Person("Britt", "kartlesing", "tulle", "smile", "lese");
+	Person p3 = new Person("Bob Europa", "kartlesing", "blindeskrift", "kappgang",
+			"naruto");
+
+	IO.println("Match med samme person:");
+	IO.println(Person.match(p1, p1));
+	IO.println();
+	IO.println("Match med p1 og p2:");
+	IO.println(Person.match(p1, p2));
+	IO.println(Person.match(p2, p1));
+	IO.println();
+	IO.println("Match med p2 og p3:");
+	IO.println(Person.match(p2, p3));
+	IO.println(Person.match(p3, p2));
+	IO.println();
+	IO.println("Match med p1 og p3:");
+	IO.println(Person.match(p1, p3));
+	IO.println(Person.match(p3, p1));
 }
--- src/Person.java
+++ src/Person.java
@@ -1,14 +1,19 @@
 public class Person {
-    String navn;
-    MengdeADT<String> hobbyer;
-
-    public Person(String navn, String ... hobbyer){
+    public JavaSetToMengde<String> hobbyer;
+    public String navn;
+    public Person(String navn, String ...hobbyer) {
         this.navn = navn;
-        this.hobbyer = new TabellMengde<>();
-        for(String hobby : hobbyer){
-            this.hobbyer.leggTil(hobby);
-        }
+        this.hobbyer = new JavaSetToMengde<>();
+        this.hobbyer.leggTil(hobbyer);
     }
 
+    static double match(Person a, Person b){
 
+        double antallFelles = a.hobbyer.snitt(b.hobbyer).antallElementer();
+        double antallKunA = a.hobbyer.minus(b.hobbyer).antallElementer();
+        double antallKunB = b.hobbyer.minus(a.hobbyer).antallElementer();
+        double antallTotalt = a.hobbyer.antallElementer() + b.hobbyer.antallElementer();
+
+        return (antallFelles - (antallKunA + antallKunB) / antallTotalt);
+    }
 }
